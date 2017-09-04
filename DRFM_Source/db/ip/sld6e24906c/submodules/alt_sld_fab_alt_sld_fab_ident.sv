0b1a564ff35ae386f6be0b284a0f572011e005c9
Misha Mesarcik <msrmic004@myuct.ac.za>
2017-09-04 17:16:38 +0200

    Report updated, Source Reconfigured

--- DRFM_Source/db/ip/sld6e24906c/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ DRFM_Source/db/ip/sld6e24906c/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 5e70990ce12bec17f698
+//   DESIGN_HASH   : 07d8c1af689178e8a07c
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h5e70990ce12bec17f698 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h07d8c1af689178e8a07c };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
